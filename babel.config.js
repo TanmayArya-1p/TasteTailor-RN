c3466ffa864b9729eba65c173efa9283bc4a7177
TanmayArya <72544600+TanmayArya-1p@users.noreply.github.com>
2024-08-26 08:58:40 +0530

    BABEL FIX

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ["nativewind/babel"],
+  plugins: ["nativewind/babel", ['@babel/plugin-transform-class-properties', { loose: true }],
+  ['@babel/plugin-transform-private-methods', { loose: true }],
+  ['@babel/plugin-transform-private-property-in-object', { loose: true }]],
 };
